adbee4d29a891712ff9a3c2aca1d3532f4e0608f
keke <243768648@qq.com>
2022-09-09 02:34:39 +0800

    feat: 崩山击第一阶段攻击及其特效

--- source/actor/state/duelist/swordman/hopsmash.lua
+++ source/actor/state/duelist/swordman/hopsmash.lua
@@ -28,23 +28,34 @@ function _HopSmash:Ctor(data, ...)
     self._ticks = data.ticks
     self._easemoveParam = data.easemove
     self._jumpParam = data.jump
-
-    -- 攻击子弹实例
-    self.bulletEntity = nil
 end
 
 function _HopSmash:Init(entity, ...)
     _Base.Init(self, entity, ...)
 
     self.entityFrameaniOriDelayList = {} -- 实体动作原始延时列表
+    self.enhanceRate = 0.0
+
+    self.endBulletEntity = nil  -- 技能结束时段子弹动作
+
+    -- 攻击子弹实例
+    self.bottomBulletEntity = nil
+    self.bottomBulletEffectList = {}
 
     self._attack = _Attack.New(self._entity)
     self._easemove = _Easemove.New(self._entity.transform, self._entity.aspect)
     -- 跳跃动作
     self._jump = _Jump.New(self._entity.transform, self._entity.aspect, function (caller, param)
-        print("_UpperSlash state jump action func excuted! param: "..param)
+        print("_HopSmash state jump action func excuted! param: "..param)
         if 3 == param then
             print("ground attack")
+
+            -- 先销毁原来的攻击子弹实例
+            if (self.endBulletEntity) then
+                self.endBulletEntity.identity.destroyProcess = 1
+                self.endBulletEntity = nil
+            end
+
             -- 落到地面后攻击
             local param = {
                 x = self._entity.transform.position.x,
@@ -53,40 +64,35 @@ function _HopSmash:Init(entity, ...)
                 direction = self._entity.transform.direction,
                 entity = self._entity
             }
-            
-            local effect1 = _FACTORY.New(self._actorDataSet[1], param)
-            local effect2 = _FACTORY.New(self._actorDataSet[2], param)
-            local effect3 = _FACTORY.New(self._actorDataSet[3], param)
-            local effect4 = _FACTORY.New(self._actorDataSet[4], param)
 
-            -- 先销毁原来的攻击子弹实例
-            if (self.bulletEntity) then
-                self.bulletEntity.identity.destroyProcess = 1
-            end
-            self.bulletEntity = _FACTORY.New(self._actorDataSet[5], param)
-
+            -- 武器刚接触地面子弹实例
+            local endOnGroundBulletEntity = _FACTORY.New(self._actorDataSet[2], param)
             self._attack:Enter(self._attackDataSet[2], self._skill.attackValues[2], _, _, true)
-            self._attack.collision[_ASPECT.GetPart(self.bulletEntity.aspect)] = "attack"
+            self._attack.collision[_ASPECT.GetPart(endOnGroundBulletEntity.aspect)] = "attack"
+
+            -- 先销毁原来的地面血气波动攻击子弹实例
+            if (self.bottomBulletEntity) then
+                self.bottomBulletEntity.identity.destroyProcess = 1
+            end
+            -- 创建的地面血气波动攻击子弹实例
+            self.bottomBulletEntity = _FACTORY.New(self._actorDataSet[7], param)
+
+            -- 销毁地面血气波动特效
+            for i, entity in pairs(self.bottomBulletEffectList) do
+                if (entity) then
+                    entity.identity.destroyProcess = 1
+                end
+            end
+            self.bottomBulletEffectList = {}
+            -- 创建地面血气波动特效
+            self.bottomBulletEffectList[1] = _FACTORY.New(self._actorDataSet[3], param)
+            self.bottomBulletEffectList[2] = _FACTORY.New(self._actorDataSet[4], param)
+            self.bottomBulletEffectList[3] = _FACTORY.New(self._actorDataSet[5], param)
+            self.bottomBulletEffectList[4] = _FACTORY.New(self._actorDataSet[6], param)
 
             _SOUND.Play(self._soundDataSet.swing)
         end
     end)
-
-    ---@param effect Actor.Entity
-    self._NewBullet = function(effect)
-        local t = effect.transform
-        local param = {
-            x = t.position.x,
-            y = t.position.y,
-            z = t.position.z,
-            direction = t.direction,
-            entity = self._entity,
-            attackValue = self._skill.attackValues[2]
-        }
-
-        _FACTORY.New(self._actorDataSet[2], param)
-        _FACTORY.New(self._actorDataSet[3], param)
-    end
 end
 
 function _HopSmash:NormalUpdate(dt, rate)
@@ -101,15 +107,32 @@ function _HopSmash:NormalUpdate(dt, rate)
         -- buff
         self._buff = _BUFF.AddBuff(self._entity, self._buffDatas)
     elseif (tick == self._ticks[2]) then
+        local param = {
+            x = self._entity.transform.position.x,
+            y = self._entity.transform.position.y,
+            z = self._entity.transform.position.z,
+            direction = self._entity.transform.direction,
+            entity = self._entity
+        }
+
+        if (self.endBulletEntity) then
+            self.endBulletEntity.identity.destroyProcess = 1
+            self.endBulletEntity = nil
+        end
+        self.endBulletEntity = _FACTORY.New(self._actorDataSet[1], param)
+
+        -- 攻击
+        self._attack:Enter(self._attackDataSet[1], self._skill.attackValues[1], _, _, true)
+        self._attack.collision[_ASPECT.GetPart(self.endBulletEntity.aspect)] = "attack"
     elseif (tick == self._ticks[3]) then
     end
 
-    -- 多段攻击
-    if (self.bulletEntity) then
-        local bulletFrameani = _ASPECT.GetPart(self.bulletEntity.aspect) ---@type Graphics.Drawable.Frameani
+    -- 第三段攻击
+    if (self.bottomBulletEntity) then
+        local bulletFrameani = _ASPECT.GetPart(self.bottomBulletEntity.aspect) ---@type Graphics.Drawable.Frameani
         local bulletTick = bulletFrameani:GetTick()
         if (3 == bulletTick) then
-            self._attack:Enter(self._attackDataSet[2], self._skill.attackValues[1], _, _, true)
+            self._attack:Enter(self._attackDataSet[2], self._skill.attackValues[2], _, _, true)
             self._attack.collision[bulletFrameani] = "attack"
         end
     end
@@ -133,22 +156,22 @@ function _HopSmash:Enter(laterState, skill)
 
     -- 获取技能准备的时间（注意：单位不是ms）
     local skillPrepareTime = self._skill:GetPrepareTime()
-    local enhanceRate = skillPrepareTime / 500
-    if 1 < enhanceRate then
-        enhanceRate = 1 -- 跳跃和移动最大增幅
+    self.enhanceRate = skillPrepareTime / 500
+    if 1 < self.enhanceRate then
+        self.enhanceRate = 1 -- 跳跃和移动最大增幅
     end
     local enhanceBaseRate = 0.7 -- 基准增幅率（用来确保增幅率始终不超过此值）
-    enhanceRate = enhanceRate * enhanceBaseRate
+    self.enhanceRate = self.enhanceRate * enhanceBaseRate
 
     local easemoveParam = self._easemoveParam
-    self._easemove:Enter("x", easemoveParam.power * (1 + enhanceRate), easemoveParam.speed, self._entity.transform.direction)
+    self._easemove:Enter("x", easemoveParam.power * (1 + self.enhanceRate), easemoveParam.speed, self._entity.transform.direction)
 
     local jumpParam = self._jumpParam
-    self._jump:Enter(jumpParam.power * (1 + enhanceRate), jumpParam.speed, jumpParam.speed * 0.3)
+    self._jump:Enter(jumpParam.power * (1 + self.enhanceRate), jumpParam.speed, jumpParam.speed * 0.3)
 
     -- 增加动画播放时间
     for n = 1, #frameaniDate.list do
-        frameaniDate.list[n].time = self.entityFrameaniOriDelayList[n] * (0.5 + enhanceRate)
+        frameaniDate.list[n].time = self.entityFrameaniOriDelayList[n] * (0.5 + self.enhanceRate)
     end
 
     _SOUND.Play(self._soundDataSet.voice)
@@ -161,10 +184,18 @@ function _HopSmash:Exit(nextState)
     
     _Base.Exit(self, nextState)
 
-    if (self._effect) then
-        self._effect.identity.destroyProcess = 1
-        self._effect = nil
+    if (self.bottomBulletEntity) then
+        self.bottomBulletEntity.identity.destroyProcess = 1
+        self.bottomBulletEntity = nil
     end
+
+    -- 销毁地面血气波动特效
+    for i, entity in pairs(self.bottomBulletEffectList) do
+        if (entity) then
+            entity.identity.destroyProcess = 1
+        end
+    end
+    self.bottomBulletEffectList = {}
 
     if (self._buff) then
         self._buff:Exit()
@@ -172,10 +203,16 @@ function _HopSmash:Exit(nextState)
 
     -- 还原动画延时
     local main = _ASPECT.GetPart(self._entity.aspect) ---@type Graphics.Drawable.Frameani
-    local frameaniDate = main:GetFrameaniData()
-    for n = 1, #frameaniDate.list do
-        frameaniDate.list[n].time = self.entityFrameaniOriDelayList[n]
+    local frameaniData = main:GetFrameaniData()
+    for n = 1, #frameaniData.list do
+        frameaniData.list[n].time = self.entityFrameaniOriDelayList[n]
     end
+
+    if (self.endBulletEntity) then
+        self.endBulletEntity.identity.destroyProcess = 1
+        self.endBulletEntity = nil
+    end
+
 end
 
 return _HopSmash
