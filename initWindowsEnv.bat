3d9867d67b6a761ce2dd288cda6623b8987c1aa2
keke <243768648@qq.com>
2024-03-08 09:27:00 +0800

    feat: 调整windows下的dpi模式为应用模式

---
+++ initWindowsEnv.bat
@@ -0,0 +1,8 @@
+% DPI缩放模式如下： %
+% 系统：----DPIUNAWARE %
+% 系统(增强)：GDIDPISCALING DPIUNAWARE %
+% 应用程序：HIGHDPIAWARE %
+set __COMPAT_LAYER=HIGHDPIAWARE
+
+::powershell (Get-WmiObject -Namespace root\cimv2 -Class Win32_VideoController).CurrentHorizontalResolution > realWindowsOsDesktopWidth.tmp
+wmic DESKTOPMONITOR where Status='ok' get ScreenWidth > realWindowsOsScreenWidth.tmp
--- start.bat
+++ start.bat
@@ -1,3 +1,2 @@
-::powershell (Get-WmiObject -Namespace root\cimv2 -Class Win32_VideoController).CurrentHorizontalResolution > realWindowsOsDesktopWidth.tmp
-wmic DESKTOPMONITOR where Status='ok' get ScreenWidth > realWindowsOsScreenWidth.tmp
+call .\initWindowsEnv.bat
 love2d-win32\love.exe .\
