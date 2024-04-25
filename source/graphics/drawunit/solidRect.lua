0cf4b30da9115be7aba93bbbc5be6373440c3fd5
keke <243768648@qq.com>
2024-04-25 23:24:40 +0800

    fix: 立体碰撞盒检测判断错误
    在判断两个碰撞盒在x z轴平面是否相交时，没有控制y轴坐标相同，没有使两个碰撞盒的xz轴平面处于同一平面

--- source/graphics/drawunit/solidRect.lua
+++ source/graphics/drawunit/solidRect.lua
@@ -1,11 +1,14 @@
 --[[
-	desc: SolidRect, its a solid rect.
-	author: Musoucrow
-	since: 2018-7-17
-	alter: 2019-6-5
-]]--
+    desc: SolidRect, its a solid rect.
+    author: Musoucrow
+    since: 2018-7-17
+    alter: 2019-6-5
+]]
+--
 
 local _Rect = require("graphics.drawunit.rect")
+
+local Table = require("lib.table")
 
 ---@class Graphics.Drawunit.SolidRect
 ---@field protected _x int
@@ -22,21 +25,21 @@ local _SolidRect = require("core.class")()
 ---@param b table<number, Graphics.Drawunit.SolidRect>
 ---@return boolean
 function _SolidRect.CollideWithList(a, b)
-	if (a == nil or b == nil) then
-		return false
-	end
+    if (a == nil or b == nil) then
+        return false
+    end
 
-	for n=1, #a do
-		for m=1, #b do
-			local isdone, x, y, z = a[n]:Collide(b[m])
+    for n = 1, #a do
+        for m = 1, #b do
+            local isdone, x, y, z = a[n]:Collide(b[m])
 
-			if (isdone) then
-				return true, x, y, z
-			end
-		end
-	end
+            if (isdone) then
+                return true, x, y, z
+            end
+        end
+    end
 
-	return false
+    return false
 end
 
 ---@param x int
@@ -46,28 +49,28 @@ end
 ---@param w int
 ---@param h int
 function _SolidRect:Ctor(x, y1, y2, z, w, h)
-	self._x = 0
-	self._y = 0
-	self._z = 0
-	self._sx = 1
-	self._sy = 1
-	self._r = 0
+    self._x = 0
+    self._y = 0
+    self._z = 0
+    self._sx = 1
+    self._sy = 1
+    self._r = 0
 
-	self._struct = {
-		x = 0,
-		y1 = 0,
-		y2 = 0,
-		z = 0,
-		w = 0,
-		h = 0
-	}
+    self._struct = {
+        x = 0,
+        y1 = 0,
+        y2 = 0,
+        z = 0,
+        w = 0,
+        h = 0
+    }
 
-	self.rectGroup = {
-		xy = _Rect.New(),
-		xz = _Rect.New()
-	}
+    self.rectGroup = {
+        xy = _Rect.New(),
+        xz = _Rect.New()
+    }
 
-	self:SetStruct(x, y1, y2, z, w, h)
+    self:SetStruct(x, y1, y2, z, w, h)
 end
 
 ---@param x int
@@ -77,24 +80,24 @@ end
 ---@param sy number
 ---@param r number
 function _SolidRect:Set(x, y, z, sx, sy, r)
-	self._x = x or self._x
-	self._y = y or self._y
-	self._z = z or self._z
-	self._sx = sx or self._sx
-	self._sy = sy or self._sy
-	self._r = r or self._r
+    self._x = x or self._x
+    self._y = y or self._y
+    self._z = z or self._z
+    self._sx = sx or self._sx
+    self._sy = sy or self._sy
+    self._r = r or self._r
 
-	self:Adjust()
+    self:Adjust()
 end
 
 ---@param name string @If it is null, that will return all values.
 ---@return number
 function _SolidRect:Get(name)
-	if (name) then
-		return self["_" .. name]
-	else
-		return self._x, self._y, self._z, self._sx, self._sy, self._r
-	end
+    if (name) then
+        return self["_" .. name]
+    else
+        return self._x, self._y, self._z, self._sx, self._sy, self._r
+    end
 end
 
 ---@param x int
@@ -104,81 +107,97 @@ end
 ---@param w int
 ---@param h int
 function _SolidRect:SetStruct(x, y1, y2, z, w, h)
-	self._struct.x = x
-	self._struct.y1 = y1
-	self._struct.y2 = y2
-	self._struct.z = z
-	self._struct.w = w
-	self._struct.h = h
+    self._struct.x = x
+    self._struct.y1 = y1
+    self._struct.y2 = y2
+    self._struct.z = z
+    self._struct.w = w
+    self._struct.h = h
 
-	self:Adjust()
+    self:Adjust()
 end
 
 ---@param name string @If it is null, that will return all values.
 ---@return number
 function _SolidRect:GetStruct(name)
-	if (name) then
-		return self._struct[name]
-	else
-		return self._struct.x, self._struct.y1, self._struct.y2, self._struct.z, self._struct.w, self._struct.h
-	end
+    if (name) then
+        return self._struct[name]
+    else
+        return self._struct.x, self._struct.y1, self._struct.y2,
+            self._struct.z, self._struct.w, self._struct.h
+    end
 end
 
 function _SolidRect:Adjust()
-	local x = self._x + self._struct.x * self._sx
-	local w = self._struct.w * math.abs(self._sx)
+    local x = self._x + self._struct.x * self._sx
+    local w = self._struct.w * math.abs(self._sx)
 
-	local y1 = self._y + self._struct.y1 * self._sy
-	local h1 = (self._struct.y2 - self._struct.y1) * math.abs(self._sy)
+    local y1 = self._y + self._struct.y1 * self._sy
+    local h1 = (self._struct.y2 - self._struct.y1) * math.abs(self._sy)
 
-	local y2 = self._y + self._z + (-self._struct.z - self._struct.h) * math.abs(self._sy)
-	local h2 = self._struct.h * math.abs(self._sy)
+    local y2 = self._y + self._z + (-self._struct.z - self._struct.h) * math.abs(self._sy)
+    local h2 = self._struct.h * math.abs(self._sy)
 
-	if (self._sx < 0) then
-		x = x - w
-	end
+    if (self._sx < 0) then
+        x = x - w
+    end
 
-	if (self._sy < 0) then
-		y1 = y1 - h1
-		y2 = y2 - h2
-	end
+    if (self._sy < 0) then
+        y1 = y1 - h1
+        y2 = y2 - h2
+    end
 
-	self.rectGroup.xy:Set(x, y1, w, h1, self._r, self._x, self._y)
-	self.rectGroup.xz:Set(x, y2, w, h2, self._r, self._x, self._y)
+    self.rectGroup.xy:Set(x, y1, w, h1, self._r, self._x, self._y)
+    self.rectGroup.xz:Set(x, y2, w, h2, self._r, self._x, self._y)
 end
 
 ---@return boolean
 function _SolidRect:IsRaw()
-	return self.rectGroup.xy:IsRaw() and self.rectGroup.xz:IsRaw()
+    return self.rectGroup.xy:IsRaw() and self.rectGroup.xz:IsRaw()
 end
 
 ---@param another Graphics.Drawunit.SolidRect
 ---@param onlyStruct boolean
 ---@return boolean
 function _SolidRect:Compare(another, onlyStruct)
-	local x, y1, y2, z, w, h = another:GetStruct()
-	local structIsComparable = self._struct.x == x and self._struct.y1 == y1 and self._struct.y2 == y2 and self._struct.z == z and self._struct.w == w and self._struct.h == h
+    local x, y1, y2, z, w, h = another:GetStruct()
+    local structIsComparable =
+        self._struct.x == x and
+        self._struct.y1 == y1 and self._struct.y2 == y2 and
+        self._struct.z == z and self._struct.w == w and self._struct.h == h
 
-	if (onlyStruct) then
-		return structIsComparable
-	else
-		local x, y, z, sx, sy, r = another:Get()
+    if (onlyStruct) then
+        return structIsComparable
+    else
+        local x, y, z, sx, sy, r = another:Get()
 
-		return structIsComparable and x == self._x and y == self._y and z == self._z and sx == self._sx and sy == self._sy and r == self._r
-	end
+        return structIsComparable and
+            x == self._x and y == self._y and
+            z == self._z and sx == self._sx and
+            sy == self._sy and r == self._r
+    end
 end
 
 ---@param solidRect Graphics.Drawunit.SolidRect
 ---@return boolean, int @isdone & x & y & z
 function _SolidRect:Collide(solidRect)
-	local xy = self.rectGroup.xy:CheckRect(solidRect.rectGroup.xy)
-	local xz, x, z = self.rectGroup.xz:CheckRect(solidRect.rectGroup.xz)
+    -- 判断x y轴平面碰撞盒是否存在相交
+    local xy = self.rectGroup.xy:CheckRect(solidRect.rectGroup.xy)
 
-	if (xy and xz) then
-		return true, x, self._y, z - self._y
-	end
+    local judgingRectY = solidRect:Get("y")
+    ---@type Graphics.Drawunit.SolidRect
+    local sameYPosSolidRect = Table.DeepClone(self)
+    sameYPosSolidRect:Set(nil, judgingRectY)
 
-	return false
+    -- 判断x z轴平面碰撞盒是否存在相交
+    local xz, x, z = sameYPosSolidRect.rectGroup.xz:CheckRect(solidRect.rectGroup.xz)
+    sameYPosSolidRect = nil
+
+    if (xy and xz) then
+        return true, x, self._y, z - self._y
+    end
+
+    return false
 end
 
 function _SolidRect:CheckPoint(x, y, z)
@@ -188,8 +207,8 @@ end
 ---@param color_xy Graphics.Drawunit.Color
 ---@param color_xz Graphics.Drawunit.Color
 function _SolidRect:Draw(color_xy, color_xz)
-	self.rectGroup.xy:Draw(color_xy)
-	self.rectGroup.xz:Draw(color_xz)
+    self.rectGroup.xy:Draw(color_xy)
+    self.rectGroup.xz:Draw(color_xz)
 end
 
 return _SolidRect
