19123f54378693b23dd983669d5aae79f411e844
keke <243768648@qq.com>
2025-01-26 23:31:11 +0800

    feat: 加载地图实例时角色初始位置

--- main.lua
+++ main.lua
@@ -67,9 +67,9 @@ function love.update(dt)
         _Update()
         _TIME.LateUpdate()
         
-        -- collectgarbage("step", 1000)
+        collectgarbage("step", 100)
     end
-    collectgarbage("step", 100)
+    -- collectgarbage("step", 100)
 end
 
 function love.draw()
