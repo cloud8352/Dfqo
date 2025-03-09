d80c993e9be5a51dff980fc0ac32c7aa9647bf1f
keke <243768648@qq.com>
2025-03-09 17:34:29 +0800

    feat: 优化技能（崩山击）手感

--- source/actor/state/duelist/swordman/hopsmash.lua
+++ source/actor/state/duelist/swordman/hopsmash.lua
@@ -14,7 +14,6 @@ local _STATE = require("actor.service.state")
 local _BUFF = require("actor.service.buff")
 local _Attack = require("actor.gear.attack")
 local EaseMove = require("actor.gear.easemove")
-local _Jump = require("actor.gear.jump")
 local InputSrv = require("actor.service.input")
 
 ---@class Actor.State.Duelist.Swordman.HopSmash : Actor.State
@@ -27,11 +26,14 @@ local _HopSmash = require("core.class")(_Base)
 ---@enum HopSmashStateProcessEnum
 local ProcessEnum = {
     Up = 1,
-    UpHold = 2,
+    HoldUp = 2,
     Down = 3,
-    DownHold = 4,
+    HoldDown = 4,
     End = 5
 }
+
+local OriTimeMsNeedUp = 40
+local OriTimeMsNeedHoldUp = 40
 
 function _HopSmash:Ctor(data, ...)
     _Base.Ctor(self, data, ...)
@@ -45,7 +47,6 @@ function _HopSmash:Init(entity, ...)
 
     self._attack = _Attack.New(self._entity)
     self._attack.element = _Attack.AttackElementStruct.Water
-    self.oriEaseMoveX = EaseMove.New(self._entity.transform, self._entity.aspect)
     self.easeMoveX = EaseMove.New(self._entity.transform, self._entity.aspect)
     self.easeMoveZ = EaseMove.New(self._entity.transform, self._entity.aspect)
 
@@ -53,72 +54,16 @@ function _HopSmash:Init(entity, ...)
 
     ---@type int
     self.process = ProcessEnum.Up
-    -- 跳跃动作
-    self._jump = _Jump.New(self._entity.transform, self._entity.aspect, function(caller, param)
-        print("_HopSmash state jump action func excuted! param: " .. param)
-        if _Jump.ProcessEnum.Up2 == param then
-            self.process = ProcessEnum.UpHold
-        end
-        if _Jump.ProcessEnum.Down1 == param then
-            self.process = ProcessEnum.Down
-            self._buff = _BUFF.AddBuff(self._entity, self._buffDatas)
-            _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.Down)
-        end
-
-
-        if _Jump.ProcessEnum.Ground == param then
-            print("ground attack")
-
-            -- 先销毁原来的攻击子弹实例
-            if (self.endBulletEntity) then
-                self.endBulletEntity.identity.destroyProcess = 1
-                self.endBulletEntity = nil
-            end
-
-            -- 落到地面后攻击
-            local param = {
-                x = self._entity.transform.position.x,
-                y = self._entity.transform.position.y,
-                z = self._entity.transform.position.z,
-                direction = self._entity.transform.direction,
-                entity = self._entity
-            }
-
-            -- 武器刚接触地面子弹实例
-            local endOnGroundBulletEntity = _FACTORY.New(self._actorDataSet[2], param)
-            -- 攻击
-            self:startAttack(self._attackDataSet[2], self._skill.attackValues[2]
-            , _ASPECT.GetPart(endOnGroundBulletEntity.aspect))
-
-            -- 创建的地面血气波动攻击子弹实例
-            local bottomBulletEntity = _FACTORY.New(self._actorDataSet[7], param)
-
-            local bottomBulletEffectList = {}
-            -- 创建地面血气波动特效
-            bottomBulletEffectList[1] = _FACTORY.New(self._actorDataSet[3], param)
-            bottomBulletEffectList[2] = _FACTORY.New(self._actorDataSet[4], param)
-            bottomBulletEffectList[3] = _FACTORY.New(self._actorDataSet[5], param)
-            bottomBulletEffectList[4] = _FACTORY.New(self._actorDataSet[6], param)
-
-            _SOUND.Play(self._soundDataSet.swing)
-
-            self.process = ProcessEnum.End
-            _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.End)
-
-            self.oriEaseMoveX:Exit()
-            self.easeMoveX:Exit()
-        end
-    end)
+    self.timeMsNeedUp = OriTimeMsNeedUp
+    self.timeMsNeedHoldUp = OriTimeMsNeedHoldUp
 end
 
 function _HopSmash:NormalUpdate(dt, rate)
     local main = _ASPECT.GetPart(self._entity.aspect) ---@type Graphics.Drawable.Frameani
 
     self._attack:Update()
-    self.oriEaseMoveX:Update(rate)
     self.easeMoveX:Update(rate)
     self.easeMoveZ:Update(rate)
-    self._jump:Update(rate)
 
     -- 持续播放空中攻击效果
     if (self.endBulletEntity and self.endBulletEntity.identity.destroyProcess == 1) then
@@ -127,26 +72,43 @@ function _HopSmash:NormalUpdate(dt, rate)
 
     if InputSrv.IsReleased(self._entity.input, self._skill:GetKey()) then
         self.canMoveXWhenKeyPressing = false
+        if self.process == ProcessEnum.Up
+            or self.process == ProcessEnum.HoldUp
+        then
+            local xPower = self.easeMoveX:GetPower()
+            self.easeMoveX:Enter("x", xPower, 1, self._entity.transform.direction)
+        end
     end
 
     if self.process == ProcessEnum.Up then
         _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.Up)
+        if self.timeMsNeedUp < 0 then
+            self.process = ProcessEnum.HoldUp
+        end
+        self.timeMsNeedUp = self.timeMsNeedUp - dt
     end
-    if self.process == ProcessEnum.UpHold then
-        _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.UpHold)
+    if self.process == ProcessEnum.HoldUp then
+        _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.HoldUp)
+        if self.timeMsNeedHoldUp < 0 then
+            self.process = ProcessEnum.Down
+            self._buff = _BUFF.AddBuff(self._entity, self._buffDatas)
+            _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.Down)
+
+            self.easeMoveZ:Enter("z", 0, -1.5, 1)
+        end
+        self.timeMsNeedHoldUp = self.timeMsNeedHoldUp - dt
     end
 
     if self.process == ProcessEnum.Down
         and main:TickEnd()
     then
-        self.process = ProcessEnum.DownHold
-        -- self.oriEaseMoveX:Exit()
-        -- self.easeMoveX:Exit()
+        self.process = ProcessEnum.HoldDown
+        self.easeMoveX:SetPower(3)
     end
 
-    if self.process == ProcessEnum.DownHold
+    if self.process == ProcessEnum.HoldDown
     then
-        _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.DownHold)
+        _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.HoldDown)
         if self.endBulletEntity == nil then
             local param = {
                 x = self._entity.transform.position.x,
@@ -163,15 +125,32 @@ function _HopSmash:NormalUpdate(dt, rate)
         end
     end
 
+    if self.process == ProcessEnum.HoldDown 
+        and self._entity.transform.position.z >= 0
+    then
+        self._entity.transform.position.z = 0
+        self._entity.transform.positionTick = 1
+        self.easeMoveX:Exit()
+        self.easeMoveZ:Exit()
+
+        self.process = ProcessEnum.End
+
+        -- 先销毁原来的攻击子弹实例
+        if (self.endBulletEntity) then
+            self.endBulletEntity.identity.destroyProcess = 1
+            self.endBulletEntity = nil
+        end
+        _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.End)
+        self:createEndEffect()
+    end
+
     if self.process == ProcessEnum.Up
-        or self.process == ProcessEnum.UpHold
+        or self.process == ProcessEnum.HoldUp
     then
         if InputSrv.IsHold(self._entity.input, self._skill:GetKey())
             and self.canMoveXWhenKeyPressing
         then
-            self.easeMoveZ:Enter("z", 5, 0.8, -1)
-            self.easeMoveX:Enter("x", 7, 0.8, self._entity.transform.direction)
-    
+            self.timeMsNeedHoldUp = self.timeMsNeedHoldUp + 11
         end
     end
 
@@ -185,19 +164,19 @@ function _HopSmash:Enter(lastState, skill)
 
     self._skill = skill
     self._attack:Exit()
-    self.oriEaseMoveX:Exit()
     self.easeMoveX:Exit()
     self.easeMoveZ:Exit()
-    self._jump:Exit()
     self._attack:Exit()
 
     self.canMoveXWhenKeyPressing = true
+    self.timeMsNeedUp = 40
+    self.timeMsNeedHoldUp = 40
 
-    self.process = ProcessEnum.Start
+    self.process = ProcessEnum.Up
     _ASPECT.Play(self._entity.aspect, self._frameaniDataSets.Up)
 
-    self.oriEaseMoveX:Enter("x", 4, 0.1, self._entity.transform.direction)
-    self._jump:Enter(11.5, 1, 1)
+    self.easeMoveX:Enter("x", 11, 0, self._entity.transform.direction)
+    self.easeMoveZ:Enter("z", 10, 0, -1)
     Util.PlaySoundByGender(self._soundDataSet, 1, self._entity.identity.gender)
 end
 
@@ -216,7 +195,6 @@ function _HopSmash:Exit(nextState)
         self.endBulletEntity.identity.destroyProcess = 1
         self.endBulletEntity = nil
     end
-
 end
 
 ---@param attackData table
@@ -225,6 +203,39 @@ end
 function _HopSmash:startAttack(attackData, attackValue, attackFrameAni)
     self._attack:Enter(attackData, attackValue, _, _, true)
     self._attack.collision[attackFrameAni] = "attack"
+end
+
+function _HopSmash:createEndEffect()
+    -- 落到地面后攻击
+    local param = {
+        x = self._entity.transform.position.x,
+        y = self._entity.transform.position.y,
+        z = self._entity.transform.position.z,
+        direction = self._entity.transform.direction,
+        entity = self._entity,
+        attackValue = {
+            damageRate = 2.01,
+            isPhysical = false
+        }
+    }
+
+    -- 武器刚接触地面子弹实例
+    local endOnGroundBulletEntity = _FACTORY.New(self._actorDataSet[2], param)
+    -- 攻击
+    self:startAttack(self._attackDataSet[2], self._skill.attackValues[2]
+    , _ASPECT.GetPart(endOnGroundBulletEntity.aspect))
+
+    -- 创建的地面血气波动攻击子弹实例
+    local bottomBulletEntity = _FACTORY.New(self._actorDataSet[7], param)
+
+    local bottomBulletEffectList = {}
+    -- 创建地面血气波动特效
+    bottomBulletEffectList[1] = _FACTORY.New(self._actorDataSet[3], param)
+    bottomBulletEffectList[2] = _FACTORY.New(self._actorDataSet[4], param)
+    bottomBulletEffectList[3] = _FACTORY.New(self._actorDataSet[5], param)
+    bottomBulletEffectList[4] = _FACTORY.New(self._actorDataSet[6], param)
+
+    _SOUND.Play(self._soundDataSet.swing)
 end
 
 return _HopSmash
