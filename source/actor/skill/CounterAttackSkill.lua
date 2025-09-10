05dcf86555e95f0143c03d12c9e2c2200508ee82
keke <243768648@qq.com>
2025-09-10 21:42:34 +0800

    feat: 可以直接通过“counterAttack”快捷键触发受身反击

---
+++ source/actor/skill/CounterAttackSkill.lua
@@ -0,0 +1,28 @@
+--[[
+	desc: CounterAttackSkill, a skill for CounterAttack.
+	author: keke
+]]--
+
+local InputSrv = require("actor.service.input")
+
+local BeatenSkill = require("actor.skill.beaten")
+
+---@class Actor.Skill.CounterAttack : Actor.Skill.Beaten
+local CounterAttackSkill = require("core.class")(BeatenSkill)
+
+---@param entity Actor.Entity
+---@param key string
+---@param data Actor.RESMGR.SkillData
+function CounterAttackSkill:Ctor(entity, key, data)
+    BeatenSkill.Ctor(self, entity, key, data)
+end
+
+function CounterAttackSkill:Update(dt)
+    BeatenSkill.Update(self, dt)
+
+    if (self:CanUse() and InputSrv.IsPressed(self._entity.input, "counterAttack")) then
+        self:Use()
+    end
+end
+
+return CounterAttackSkill
