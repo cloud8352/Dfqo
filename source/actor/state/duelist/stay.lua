eee164aec208bd8eebe9e0fb55426dab077651a7
keke <243768648@qq.com>
2025-10-12 21:01:21 +0800

    feaet: MultiStepMove Ai

--- source/actor/state/duelist/stay.lua
+++ source/actor/state/duelist/stay.lua
@@ -20,8 +20,6 @@ local _Stay = require("core.class")(_Base)
 function _Stay:Ctor(data, param, name)
     _Base.Ctor(self, data, param, name)
 
-    -- 之前是否是 行走
-    self.isMovingAFewTimeAgo = false
     -- 行走结束时间
     self.movingEndTime = 0
     -- 行走到奔跑之间的响应时间
@@ -35,42 +33,43 @@ function _Stay:NormalUpdate(dt, rate)
     if self._entity.states.later then
         if self._entity.states.later:GetName() ~= self.lastStateName then
             self.lastStateName = self._entity.states.later:GetName()
-            -- print(lastStateName)
             if self.lastStateName == "move" then
-                -- print("last frame move")
                 self.movingEndTime = _TIME.GetTime()
             end
         end
     end
 
-    -- 判断是否为 奔跑
-    if _TIME.GetTime() - self.movingEndTime > self.movingReactTimeS then
-        self.isMovingAFewTimeAgo = false
-    else
-        self.isMovingAFewTimeAgo = true
-    end
-    if self.isMovingAFewTimeAgo then
-        if self._entity.transform.direction == -1 and
-            _INPUT.IsHold(self._entity.input, "left") then
-            _STATE.Play(self._entity.states, "run")
-            self.lastStateName = "run"
-            return
+    -- 判断是否按下了方向键
+    if _INPUT.IsPressed(self._entity.input, "left")
+        or _INPUT.IsPressed(self._entity.input, "right")
+        or _INPUT.IsPressed(self._entity.input, "up")
+        or _INPUT.IsPressed(self._entity.input, "down")
+        or _INPUT.IsHold(self._entity.input, "left")
+        or _INPUT.IsHold(self._entity.input, "right")
+        or _INPUT.IsHold(self._entity.input, "up")
+        or _INPUT.IsHold(self._entity.input, "down")
+    then
+        local nextStateName = self._nextState
+        -- 判断是否为 奔跑
+        if _TIME.GetTime() - self.movingEndTime < self.movingReactTimeS then
+            if self._entity.transform.direction == -1 and
+                _INPUT.IsPressed(self._entity.input, "left") then
+                nextStateName = "run"
+            end
+            if self._entity.transform.direction == 1 and
+                _INPUT.IsPressed(self._entity.input, "right") then
+                nextStateName = "run"
+            end
+        else
+            -- 行走
         end
-        if self._entity.transform.direction == 1 and
-            _INPUT.IsHold(self._entity.input, "right") then
-            _STATE.Play(self._entity.states, "run")
-            self.lastStateName = "run"
-            return
+        
+        if false == _STATE.HasState(self._entity.states, "run") then
+            nextStateName = self._nextState
         end
-    end
 
-    -- 判断是否为 行走
-    for n=1, #_CONFIG.arrow do
-        if (_INPUT.IsHold(self._entity.input, _CONFIG.arrow[n])) then
-            _STATE.Play(self._entity.states, self._nextState)
-            self.lastStateName = "stay"
-            return
-        end
+        _STATE.Play(self._entity.states, nextStateName)
+        self.lastStateName = "stay"
     end
 end
 
